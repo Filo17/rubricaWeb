6e8069f453cb4b81b55587969181b9ae01a66065
Filippo Ambrogiani <81953792+Filo17@users.noreply.github.com>
2024-03-13 10:52:42 +0100

    Add files via upload

---
+++ Ambrogiani.Filippo.4i.rubricaWeb/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Ambrogiani.Filippo._4i.rubricaWeb.Models;
+
+namespace Ambrogiani.Filippo._4i.rubricaWeb.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        
+        List<string> lista = new();
+        lista.Add("Pecora");
+        lista.Add("Sabbia");
+        lista.Add("Fragole");
+        return View( lista );
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+    public IActionResult Login()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
